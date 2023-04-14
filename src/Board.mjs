a13ba418996487812092d2906f472e23db7f6c4f
Joey <josephdguillaume@gmail.com>
2023-04-14 11:54:31 -0400

    refactor: toString, feat: drop implemented

--- src/Board.mjs
+++ src/Board.mjs
@@ -5,17 +5,30 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.fallingBlock = null;
   }
 
   static EMPTY = ".";
   static SPACE = "\n";
 
+  drop(block) {
+    this.fallingBlock = block;
+  }
+
+  hasFalling() {
+    return this.fallingBlock !== null;
+  }
+
+  hasFallingAt(row, col) {
+    return this.hasFalling() && (row === 0) & (col === 1);
+  }
+
   toString() {
     let state = "";
 
-    for (let rows = 0; rows < this.height; rows++) {
-      for (let cols = 0; cols < this.width; cols++) {
-        state += Board.EMPTY;
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        state += this.hasFallingAt(row, col) ? this.fallingBlock : Board.EMPTY;
       }
 
       state += Board.SPACE;
--- test/FallingBlocks.test.mjs
+++ test/FallingBlocks.test.mjs
@@ -15,7 +15,6 @@ describe("Falling blocks", () => {
     );
   });
 
-  /*
   describe("When a block is dropped", () => {
     beforeEach(() => {
       board.drop("X");
@@ -46,7 +45,6 @@ describe("Falling blocks", () => {
       expect(after).to.equal(before);
     });
   });
-  */
 
   /*
   describe("When a block reaches the bottom", () => {
